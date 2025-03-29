72c71e93d1cd531766fa559eff2665ee3897a154
Atharv Nakti <128978076+atharv3434@users.noreply.github.com>
2025-03-29 23:11:25 +0530

    Add files via upload

---
+++ PasswordGenerator.java
@@ -0,0 +1,17 @@
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}";
+    private static final int PASSWORD_LENGTH = 12;
+
+    public static String generateStrongPassword() {
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+            password.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+
+        return password.toString();
+    }
+}
